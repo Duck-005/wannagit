8119b429fd355370e60aec077a58f6874300d1a0
Duck-005 <pondesai87@gmail.com>
2025-09-01 01:49:25 +0530

    changed log command so that it creates the log.dot file directly instead of printing to console

--- cmd/log.go
+++ cmd/log.go
@@ -2,22 +2,24 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
-	"github.com/spf13/cobra"
 	"github.com/Duck-005/wannagit/utils"
+	"github.com/spf13/cobra"
 )
 
-func logGraphviz(repo utils.Repo, sha string, seenSet map[string]bool) {
+func logGraphviz(repo utils.Repo, sha string, seenSet map[string]bool, log string) (string, error) {
+	var err error
 	if seenSet[sha] {
-		return
+		return log, err
 	}
 	seenSet[sha] = true
 
 	object := utils.ObjectRead(repo, sha)
 	commit, ok := object.(*utils.GitCommit)
 	if !ok {
-		return
+		return "", fmt.Errorf("error reading commit object: %v", sha)
 	}
 	message := string(commit.Data[""][0])
 	message = strings.ReplaceAll(message, "\\", "\\\\")
@@ -27,17 +29,22 @@ func logGraphviz(repo utils.Repo, sha string, seenSet map[string]bool) {
 		message = message[:strings.Index(message, "\n")]
 	}
 
-	fmt.Printf(" c_%v [label=\"%v: %v\"]", sha, sha[0:7], message)
-	
+	log += 	fmt.Sprintf(" c_%v [label=\"%v: %v\"]", sha, sha[0:7], message)
+
 	if _, ok := commit.Data["parent"]; !ok {
 		// base case: the initial commit
-		return
+		return log, err
 	}
 
 	for _, parent := range commit.Data["parent"] {
-		fmt.Printf(" c_%v -> c_%v;", sha, parent)
-		logGraphviz(repo, parent, seenSet)
+		log += fmt.Sprintf(" c_%v -> c_%v;", sha, parent)
+		l, err := logGraphviz(repo, parent, seenSet, log)
+		if err != nil {
+			return "", err
+		}
+		log = l
 	}
+	return log, err
 }
 
 var logCmd = &cobra.Command{
@@ -53,12 +60,16 @@ var logCmd = &cobra.Command{
 
 		repo := utils.RepoFind(".", true)
 
-		fmt.Print("digraph wannagitLog{\n")
-		fmt.Print("	node[shape=rect]")
+		log := ""
+		log += "digraph wannagitLog{node[shape=rect]"
+		l, err := logGraphviz(repo, utils.ObjectFind(repo, args[0], "", false), make(map[string]bool), log)
+		if err != nil {
+			utils.ErrorHandler("error in getting log data", err)
+			return
+		}
+		l += "}"
 
-		logGraphviz(repo, utils.ObjectFind(repo, args[0], "", false), make(map[string]bool))
-
-		fmt.Print("}")
+		os.WriteFile("log.dot", []byte(l), os.ModePerm)
 	},
 }
 
